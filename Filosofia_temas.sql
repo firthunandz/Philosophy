81edd10ef2e9629a09d9884577f26c3792a4116e
firthunandz <fernandonchiesa@gmail.com>
2023-01-10 20:57:28 -0300

    temas 1

---
+++ Filosofia_temas.sql
@@ -0,0 +1,68 @@
+use filosofia;
+
+create table temas(
+tema_id int not null auto_increment primary key,
+tema varchar(50) not null,
+descripcion text(10000)
+);
+
+insert into temas(tema) values ('Monismo');
+insert into temas(tema) values ('Pluralismo');
+insert into temas(tema) values ('Atomismo');
+insert into temas(tema) values ('Hilozoismo');
+insert into temas(tema) values ('Antropologia');
+insert into temas(tema) values ('Cosmologia');
+insert into temas(tema) values ('Sofistas');
+insert into temas(tema) values ('Patrística');
+insert into temas(tema) values ('Escolástica');
+insert into temas(tema) values ('Neoplatonismo');
+insert into temas(tema) values ('Lógica aristotélica');
+insert into temas(tema) values ('Metafísica');
+insert into temas(tema) values ('Estoicismo');
+insert into temas(tema) values ('Epicureísmo');
+insert into temas(tema) values ('Teología');
+insert into temas(tema) values ('Dualismo');
+insert into temas(tema) values ('Lógica');
+insert into temas(tema) values ('Empirismo');
+insert into temas(tema) values ('Averroísmo');
+insert into temas(tema) values ('Humanismo');
+insert into temas(tema) values ('Epistemología');
+insert into temas(tema) values ('Idealismo');
+insert into temas(tema) values ('Neokantismo');
+insert into temas(tema) values ('Existencialismo');
+insert into temas(tema) values ('Ateísmo');
+insert into temas(tema) values ('Positivismo');
+insert into temas(tema) values ('Pesimismo');
+insert into temas(tema) values ('Utilitarismo');
+insert into temas(tema) values ('Materialismo');
+insert into temas(tema) values ('Materialismo dialéctico');
+insert into temas(tema) values ('Nihilismo');
+insert into temas(tema) values ('Pragmatismo');
+insert into temas(tema) values ('Liberalismo');
+insert into temas(tema) values ('Anarcocomunismo');
+insert into temas(tema) values ('Darwinismo social');
+insert into temas(tema) values ('Lógica matemática');
+insert into temas(tema) values ('Lógica moderna');
+insert into temas(tema) values ('Racionalismo');
+insert into temas(tema) values ('Positivismo lógico');
+insert into temas(tema) values ('Fenomenología');
+insert into temas(tema) values ('Estructuralismo');
+insert into temas(tema) values ('Postestructuralismo');
+insert into temas(tema) values ('Materialismo filosófico');
+insert into temas(tema) values ('Actualismo');
+insert into temas(tema) values ('Neotomismo');
+insert into temas(tema) values ('Lenguaje');
+insert into temas(tema) values ('Modernismo');
+insert into temas(tema) values ('Hermenéutica');
+insert into temas(tema) values ('Deconstrucción');
+insert into temas(tema) values ('Feminismo francés');
+insert into temas(tema) values ('Teoría psicoanalítica');
+insert into temas(tema) values ('Teoría crítica');
+insert into temas(tema) values ('Marxismo');
+insert into temas(tema) values ('Historicismo');
+insert into temas(tema) values ('Metafilosofía');
+insert into temas(tema) values ('Psicoanálisis');
+insert into temas(tema) values ('Lingüística');
+insert into temas(tema) values ('Crítica literaria');
+insert into temas(tema) values ('Gnoseología');
+insert into temas(tema) values ('Nominalismo');
